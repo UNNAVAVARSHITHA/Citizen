f0ae1e8c7f6c0426bc3bec9f29db9833d4b0d86c
ambarisham <ambarisha.malladi@gmail.com>
2024-09-05 10:39:25 +0530

    first commit

---
+++ src/main/java/com/testing/MarkscalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.testing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarkscalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarkscalculatorApplication.class, args);
+	}
+
+}
